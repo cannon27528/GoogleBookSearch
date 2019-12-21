ecf7703ea28c4231f56a638a0918ee06c8860b7d
Chase Cannon <chase27cannon52@gmail.com>
2019-12-21 13:07:54 -0600

    populated database

--- models/book.js
+++ models/book.js
@@ -1,10 +1,29 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const bookSchema = new Schema({
-  TODO: //SEE homework instruction number 4
+var mongoose = require("mongoose");
+// Save a reference to the Schema constructor
+var Schema = mongoose.Schema;
+// Using the Schema constructor, create a new UserSchema object
+// This is similar to a Sequelize model
+var BookSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  authors: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String
+  },
+  link: {
+    type: String
+  }
 });
-
-const Book = mongoose.model("Book", bookSchema);
-
+// This creates our model from the above schema, using mongoose's model method
+var Book = mongoose.model("Book", BookSchema);
+// Export the Article model
 module.exports = Book;
