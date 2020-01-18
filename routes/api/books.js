32c94b75820d17a4d3ab51efa55d6a3dc03315ae
Chase Cannon <chase27cannon52@gmail.com>
2020-01-18 13:07:55 -0600

    experiment

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,13 +1,22 @@
+// const router = require("express").Router();
+// const booksController = require("../../controllers/googleController");
+
+// // Matches with "/api/google"
+// router.route("/")
+//   .get(booksController.findAll)
+//   .post(booksController.create)
+// router.route("/:id")
+// .get(booksController.findbyID)
+//   .put(booksController.update)
+//   .delete(booksController.remove)
+
+// module.exports = router;
 const router = require("express").Router();
-const booksController = require("../../controllers/googleController");
+const bookController = require("../../controllers/googleController");
 
 // Matches with "/api/google"
-router.route("/")
-  .get(booksController.findAll)
-  .post(booksController.create)
-router.route("/:id")
-.get(booksController.findbyID)
-  .put(booksController.update)
-  .delete(booksController.remove)
+router
+  .route("/")
+  .get(bookController.findAll);
 
 module.exports = router;
