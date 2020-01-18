79b747c80926ba45264692ea211fa82a4790aa2d
Chase Cannon <chase27cannon52@gmail.com>
2020-01-18 09:51:10 -0600

    run build

---
+++ client/build/precache-manifest.c8d422f14a89c94f9342e05358d9b0a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee89f2dbf32b47c00225",
+    "url": "/static/css/main.518ae91e.chunk.css"
+  },
+  {
+    "revision": "ee89f2dbf32b47c00225",
+    "url": "/static/js/main.6977f972.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "79b71721221530e139cc",
+    "url": "/static/js/2.7a05ecf7.chunk.js"
+  },
+  {
+    "revision": "4ce99fc5ac7b172bfeda5e33489f9598",
+    "url": "/index.html"
+  }
+];
